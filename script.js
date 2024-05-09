dc04b7c13e3ec18b7bde8ef45a769fa3c2a0fe74
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-05-09 15:00:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function fibonacci(num) {
+function fibonacci(num) 
+{
 // your code here
+	let arr=[];
+
+	arr[0]=0;
+	arr[1]=1;
+
+	for(let i=2;i<num;i++)
+		{
+			arr[i]=arr[i-1]+arr[i-2];
+		}
+	return arr[num-1];
 }
 
 module.exports = fibonacci;
